10cf7c3e071083448132d1bd21af022902e0cfaa
Jesung Yang <y.j3ms.n@gmail.com>
2024-09-01 22:06:08 +0900

    add bunch of scripts

---
+++ src/run-Oz.sh
@@ -0,0 +1,6 @@
+./simple-build-ir-Oz-povray_r-511.sh
+
+./asm_gen_non-mod-Oz.sh
+./asm_gen_mod-Oz.sh
+
+./compare_s.sh > result-Oz
---
+++ src/run.sh
@@ -0,0 +1,6 @@
+./simple-build-ir-povray_r-511.sh
+
+./asm_gen_non-mod.sh
+./asm_gen_mod.sh
+
+./compare_s.sh > result-O3
